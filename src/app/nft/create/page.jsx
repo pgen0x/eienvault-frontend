03f92753fe7662b3763554c3c68b456666366b2f
Ilham Nst <protogenox@gmail.com>
2023-09-01 16:58:09 +0700

    fix: Handle fetch error gracefully and show error message
    In the `Page()` function of `page.jsx`, added error handling for the `fetch` request to the API endpoint. If the network response is not ok, an error is thrown and caught. An error message is logged to the console and can be used to show an error message to the user.

--- src/app/nft/create/page.jsx
+++ src/app/nft/create/page.jsx
@@ -4,13 +4,26 @@ import Create from './create';
 const accounts = ['0x30756...Fb179', '0x30756...Zi57G', '0x30756...Gy352'];
 
 export default async function Page() {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/chain/getall`,
-    {
-      cache: 'no-store',
-    },
-  );
-  const data = await res.json();
+  let data = [];
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/chain/getall`,
+      {
+        cache: 'force-cache',
+      },
+    );
+
+    if (!res.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    data = await res.json();
+
+    // Continue with your code
+  } catch (error) {
+    console.error('Fetch failed:', error);
+    // Handle the error gracefully, e.g., show an error message to the user
+  }
   return (
     <>
       <div className="container m-auto mb-5">

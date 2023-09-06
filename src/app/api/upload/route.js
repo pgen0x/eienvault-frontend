acb857969da9fa9d936551df5de190e4ff63ca50
Ilham Nst <protogenox@gmail.com>
2023-09-06 19:21:19 +0700

    feat: update file upload functionality
    - Retrieve uploaded file name from FormData
    - Generate new file name with extension
    - Save uploaded file to specified location in public folder
    - Return success response with new file name

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -4,20 +4,22 @@ import { NextResponse } from 'next/server';
 export async function POST(request) {
   const data = await request.formData();
   const file = data.get('file');
-  console.log(file);
+  const fileName = data.get('filename');
 
   if (!file) {
     return NextResponse.json({ success: false });
   }
+
+  const fileExtension = file.name.split('.').pop();
+  const newFileName = `${fileName}.${fileExtension}`;
 
   const bytes = await file.arrayBuffer();
   const buffer = Buffer.from(bytes);
 
   // With the file data in the buffer, you can do whatever you want with it.
   // For this, we'll just write it to the filesystem in a new location
-  const path = `/tmp/${file.name}`;
+  const path = `./public/uploads/collections/${newFileName}`;
   await writeFile(path, buffer);
-  console.log(`open ${path} to see the uploaded file`);
 
-  return NextResponse.json({ success: true });
+  return NextResponse.json({ success: true, newFileName });
 }

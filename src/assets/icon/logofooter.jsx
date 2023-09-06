83ad9af0bb4609a53b0c31cc9196977b4b492572
Ilham Nst <protogenox@gmail.com>
2023-09-06 19:23:03 +0700

    fix: rename classname icons

--- src/assets/icon/logofooter.jsx
+++ src/assets/icon/logofooter.jsx
@@ -2,9 +2,27 @@ import React from 'react';
 
 const LogoFooter = () => {
   return (
-    <svg width="29" height="30" viewBox="0 0 29 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect y="0.852051" width="28.2958" height="28.2958" rx="4.71596" fill="#32A9FF" />
-      <path d="M20.6323 7.33667H14.1382V14.6834M14.1382 14.5568V22.6635H8.25292" stroke="white" stroke-width="2.35798" stroke-linecap="round" stroke-linejoin="round" />
+    <svg
+      width="29"
+      height="30"
+      viewBox="0 0 29 30"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        y="0.852051"
+        width="28.2958"
+        height="28.2958"
+        rx="4.71596"
+        fill="#32A9FF"
+      />
+      <path
+        d="M20.6323 7.33667H14.1382V14.6834M14.1382 14.5568V22.6635H8.25292"
+        stroke="white"
+        strokeWidth="2.35798"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
     </svg>
   );
 };

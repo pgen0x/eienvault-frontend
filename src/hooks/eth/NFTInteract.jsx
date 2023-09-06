f63a58a25b2525bbad1623a0bdef32740bb168e0
Ilham Nst <protogenox@gmail.com>
2023-09-06 15:34:39 +0700

    fix: eth hooks functionality

--- src/hooks/eth/NFTInteract.jsx
+++ src/hooks/eth/NFTInteract.jsx
@@ -1,15 +1,14 @@
 import { ethers } from 'ethers';
-import { getEthersProvider } from './provider';
-import { getEthersSigner } from './signer';
-import abi from './Artifacts/NFT';
+import { useEthersProvider } from './provider';
+import { useEthersSigner } from './signer';
+import abi from './Artifacts/NFT_Abi';
 import nftBytecode from '@/hooks/eth/Artifacts/NFT_Bytecode';
 
-const provider = getEthersProvider();
 let NFTToken;
 
 const getNFT = async (_contractAddress) => {
   try {
-    const token = new ethers.Contract(_contractAddress, abi, provider);
+    const token = new ethers.Contract(_contractAddress, abi);
     NFTToken = token;
     return token;
   } catch (err) {
@@ -19,7 +18,7 @@ const getNFT = async (_contractAddress) => {
 
 const mints = async (_user, tokenURI, _royalties) => {
   try {
-    const signer = getEthersSigner(); //ethers.getSigners();;
+    const signer = useEthersSigner(); //ethers.getSigners();;
     const data = await nFT.mint.staticCall(_user, tokenURI, _royalties);
     const tx = await NFTToken.connect(signer).mint(_user, tokenURI, _royalties);
     tx.wait();
@@ -31,7 +30,7 @@ const mints = async (_user, tokenURI, _royalties) => {
 
 const royaltyInfoS = async (_tokenId, _sellPrice) => {
   try {
-    const signer = provider.getSigner();
+    const signer = useEthersSigner();
     const tx = await NFTToken.connect(signer).royaltyInfo(_tokenId, _sellPrice);
     tx.wait();
   } catch (err) {
@@ -41,7 +40,7 @@ const royaltyInfoS = async (_tokenId, _sellPrice) => {
 
 const deployNFT = async function (_name, _symbol, _contractURI) {
   if (window.ethereum) {
-    const signer = provider.getSigner();
+    const signer = useEthersSigner();
     try {
       const factory = new ethers.ContractFactory(abi, nftBytecode, signer);
       const contract = await factory.deploy(_name, _symbol, _contractURI);

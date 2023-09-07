b704feb297d9046fc036bdce58e9a3e8fa2937f5
Ilham Nst <protogenox@gmail.com>
2023-09-07 10:54:04 +0700

    feat: Add getUserInformation function
    This commit adds the `getUserInformation` function to fetch user information from the API using the provided token. The fetched data is stored in the `dataUser` state variable.

--- src/hooks/AuthContext.js
+++ src/hooks/AuthContext.js
@@ -5,6 +5,7 @@ const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
   const [token, setToken] = useState(null);
+  const [dataUser, setDataUser] = useState([]);
   const { isConnected, address } = useAccount();
 
   const login = async (newToken) => {
@@ -46,14 +47,42 @@ export function AuthProvider({ children }) {
 
       const data = await response.json();
       await login(data.token);
+      await getUserInformation(data.token);
     } catch (error) {
       logout();
       console.error('Login error:', error.message);
     }
   };
 
+  const getUserInformation = async (datatoken) => {
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/user/me`,
+        {
+          method: 'GET',
+
+          headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${datatoken}`,
+            'Content-Type': 'application/json', // Set the content type to JSON
+          },
+        },
+      );
+
+      if (!response.ok) {
+        throw new Error('Login request failed');
+      }
+
+      const data = await response.json();
+      console.log('data', data);
+      setDataUser(data);
+    } catch (error) {
+      console.error('Get data failed:', error.message);
+    }
+  };
+
   return (
-    <AuthContext.Provider value={{ token, login, logout }}>
+    <AuthContext.Provider value={{ token, dataUser, login, logout }}>
       {children}
     </AuthContext.Provider>
   );

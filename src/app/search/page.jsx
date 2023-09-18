b735e9ae0be3c50e5c98f7fff6604ee21b86d3dd
Ilham Nst <protogenox@gmail.com>
2023-09-18 17:38:14 +0700

    fix: rename class component

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -1,41 +1,38 @@
-'use client'
+'use client';
 
-import { useEffect, useState } from "react";
-import Collection from "../collection/page";
-import NftPage from "../nft/page";
+import { useEffect, useState } from 'react';
+import Collection from '../collection/page';
+import NftPage from '../nft/page';
 
-const searchPage = () => {
-  const [groupOptions, setGroupOptions] = useState("collection");
+const SearchPage = () => {
+  const [groupOptions, setGroupOptions] = useState('collection');
 
   return (
     <>
       <div className="container mx-auto">
         <section>
-          <ul className="mt-5 flex gap-3 justify-start items-center text-black">
+          <ul className="mt-5 flex items-center justify-start gap-3 text-black">
             <li
-              className={`${groupOptions === "collection" ? 'font-bold' : ''}`}
-              onClick={() => setGroupOptions("collection")}>
-              <button onClick={() => setGroupOptions("user")}>Collections</button>
+              className={`${groupOptions === 'collection' ? 'font-bold' : ''}`}
+              onClick={() => setGroupOptions('collection')}
+            >
+              <button onClick={() => setGroupOptions('user')}>
+                Collections
+              </button>
             </li>
-            <li
-              className={`${groupOptions === "nft" ? 'font-bold' : ''}`} >
-              <button onClick={() => setGroupOptions("nft")}>NFTs</button>
+            <li className={`${groupOptions === 'nft' ? 'font-bold' : ''}`}>
+              <button onClick={() => setGroupOptions('nft')}>NFTs</button>
             </li>
-            <li
-              className={`${groupOptions === "user" ? 'font-bold' : ''}`} >
-              <button onClick={() => setGroupOptions("user")}>User</button>
+            <li className={`${groupOptions === 'user' ? 'font-bold' : ''}`}>
+              <button onClick={() => setGroupOptions('user')}>User</button>
             </li>
           </ul>
         </section>
-        <section>
-          {groupOptions == "collection" && <Collection />}
-        </section>
-        <section>
-          {groupOptions == "nft" && <NftPage />}
-        </section>
+        <section>{groupOptions == 'collection' && <Collection />}</section>
+        <section>{groupOptions == 'nft' && <NftPage />}</section>
       </div>
     </>
   );
-}
+};
 
-export default searchPage;
+export default SearchPage;

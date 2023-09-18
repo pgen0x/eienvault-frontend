5eb72e7742c2b9726d2f81645a8e07749f3e4146
Ilham Nst <protogenox@gmail.com>
2023-09-18 17:31:27 +0700

    feat: Add upcoming auction countdown component

---
+++ src/components/slideshow/upcomingcountdown.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import moment from 'moment';
+
+function UpcomingCountdown({ endDate }) {
+  const [countdown, setCountdown] = useState(calculateCountdown());
+
+  function calculateCountdown() {
+    const now = moment();
+    const end = moment(endDate * 1000); // Convert to milliseconds
+    const duration = moment.duration(end.diff(now));
+    return duration;
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCountdown(calculateCountdown());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <>
+      <div className="w-12 rounded-lg bg-white bg-opacity-40 py-3 text-white">
+        {countdown.days()}d
+      </div>
+      <div className="w-12 rounded-lg bg-white bg-opacity-40 py-3 text-white">
+        {countdown.hours()}h
+      </div>
+      <div className="w-12 rounded-lg bg-white bg-opacity-40 py-3 text-white">
+        {countdown.minutes()}m
+      </div>
+      <div className="w-12 rounded-lg bg-white bg-opacity-40 py-3 text-white">
+        {countdown.seconds()}s
+      </div>
+    </>
+  );
+}
+
+export default UpcomingCountdown;

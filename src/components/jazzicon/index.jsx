ba715ff71dd28ca256665f75acfb3297cdbed791
Ilham Nst <protogenox@gmail.com>
2023-09-29 17:17:08 +0700

    chore: Refactor JazzIcon component and improve color generation
    - Refactor JazzIcon component and import color from 'tinycolor2'
    - Add function to generate a three-color gradient based on seed
    - Improve color generation by using tinycolor2 library
    - Adjust color stops of the gradient to create a visually pleasing effect

--- src/components/jazzicon/index.jsx
+++ src/components/jazzicon/index.jsx
@@ -1,5 +1,6 @@
 import jazzicon from '@metamask/jazzicon';
 import React, { useEffect, useRef } from 'react';
+import color from 'tinycolor2';
 
 export function JazzIcon({ diameter, seed, useGradientFallback }) {
   const ref = useRef(null);
@@ -21,21 +22,22 @@ export function JazzIcon({ diameter, seed, useGradientFallback }) {
     }
   }, [diameter, seed, useGradientFallback]);
 
-  // Function to generate a consistent gradient image based on seed
+  // Function to generate a consistent three-color gradient image based on seed
   const generateConsistentGradient = (size, seed) => {
     // Generate colors based on the seed with increased saturation and lightness
-    const color1 = generateColor(seed, 0, 90, 50); // Increase saturation and lightness
-    const color2 = generateColor(seed, 1, 10, 80); // Increase saturation and lightness
+    const n = seed;
+    const c1 = color({ h: n % 360, s: 0.95, l: 0.5 });
+    const c2 = c1.triad()[1];
 
     const canvas = document.createElement('canvas');
     canvas.width = size;
     canvas.height = size;
     const ctx = canvas.getContext('2d');
 
-    // Create a gradient based on the colors with adjusted color stops
+    // Create a gradient based on the three colors with adjusted color stops
     const gradient = ctx.createLinearGradient(0, 0, size, size);
-    gradient.addColorStop(0.2, color1); // Adjust the position of the first color
-    gradient.addColorStop(0.8, color2); // Adjust the position of the second color
+    gradient.addColorStop(0.2, c1.toHexString()); // Start with color c1
+    gradient.addColorStop(0.9, c2.toHexString()); // Middle color c2
 
     ctx.fillStyle = gradient;
     ctx.fillRect(0, 0, size, size);
@@ -44,7 +46,12 @@ export function JazzIcon({ diameter, seed, useGradientFallback }) {
   };
 
   // Function to generate a color based on seed, index, saturation, and lightness
-  const generateColor = (seed = 0, index = 0, saturation = 50, lightness = 50) => {
+  const generateColor = (
+    seed = 0,
+    index = 0,
+    saturation = 50,
+    lightness = 50,
+  ) => {
     const hue = (seed * 1000 + index * 100) % 360; // Use the seed to generate hue
     return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
   };

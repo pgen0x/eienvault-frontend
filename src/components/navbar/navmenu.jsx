57d7a436010d4267c4067e59e00e473936c818f1
Arief <arief.siswanto@snapinnovations.io>
2023-10-02 16:34:18 +0700

    - Update structure nav menu to hover css

--- src/components/navbar/navmenu.jsx
+++ src/components/navbar/navmenu.jsx
@@ -1,38 +1,23 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown, faSearch } from '@fortawesome/free-solid-svg-icons';
-import { useEffect, useRef } from 'react';
-import { useState } from 'react';
 import { useRouter } from 'next-nprogress-bar';
 const NavMenu = () => {
   const router = useRouter();
-  const [openMenu, setOpenMenu] = useState({ trending: false, stats: false });
-  const handleOpenMenu = (menu) => {
-    let processOpenMenu = openMenu;
-    for (const key in processOpenMenu) {
-      if (key == menu) {
-        processOpenMenu[key] = !processOpenMenu[key];
-      } else {
-        processOpenMenu[key] = false;
-      }
-    }
-    setOpenMenu({ ...openMenu, processOpenMenu });
-  };
 
   return (
     <div className="hidden w-1/6 items-center justify-start sm:hidden md:hidden lg:inline-flex xl:inline-flex 2xl:inline-flex">
       <div className="flex w-28 items-center justify-center gap-2 rounded-lg px-4 py-2">
-        <div className="relative">
+        <div className="group/discover relative">
           <button
             type="button"
             className="flex items-center gap-x-1 text-sm font-bold leading-tight text-primary-500 dark:text-white"
             aria-expanded="false"
-            onClick={() => handleOpenMenu('trending')}
           >
             Discover
             <FontAwesomeIcon icon={faChevronDown} />
           </button>
-          {openMenu.trending && (
-            <div className="absolute -left-6 top-full z-10 mt-3 w-48 overflow-hidden rounded-xl bg-white shadow-lg ring-1 ring-gray-900/5">
+          <div className="invisible absolute -left-6 top-full z-10 w-48 overflow-hidden pt-3 group-hover/discover:visible">
+            <div className="rounded-xl bg-white shadow-lg ring-1 ring-gray-900/5">
               <div className="p-3">
                 <div className="group relative flex items-center gap-x-6 rounded-lg p-2 text-sm leading-6 hover:bg-gray-50">
                   <div className="flex-auto">
@@ -66,22 +51,21 @@ const NavMenu = () => {
                 </div> */}
               </div>
             </div>
-          )}
+          </div>
         </div>
       </div>
       <div className="flex w-28 items-center justify-center gap-2 rounded-lg px-4 py-2">
-        <div className="relative">
+        <div className="group/discover relative">
           <button
             type="button"
             className="flex items-center gap-x-1 text-sm font-bold leading-tight text-primary-500 dark:text-white"
             aria-expanded="false"
-            onClick={() => handleOpenMenu('stats')}
           >
             Stats
             <FontAwesomeIcon icon={faChevronDown} />
           </button>
-          {openMenu.stats && (
-            <div className="absolute -left-6 top-full z-10 mt-3 w-48 overflow-hidden rounded-xl bg-white shadow-lg ring-1 ring-gray-900/5">
+          <div className="invisible absolute -left-6 top-full z-10 w-48 overflow-hidden pt-3 group-hover/discover:visible">
+            <div className="rounded-xl bg-white shadow-lg ring-1 ring-gray-900/5">
               <div className="p-3">
                 <div className="group relative flex items-center gap-x-6 rounded-lg p-2 text-sm leading-6 hover:bg-gray-50">
                   <div className="flex-auto">
@@ -115,7 +99,7 @@ const NavMenu = () => {
               </div> */}
               </div>
             </div>
-          )}
+          </div>
         </div>
       </div>
     </div>

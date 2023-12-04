d1afcb7902406d1760ba72617d45eec4ddd70678
Ilham Nst <protogenox@gmail.com>
2023-12-04 10:16:13 +0700

    feat: Add modal functionality for rescuing ERC-721 and ERC-20 tokens
    - This commit adds modal functionality to the MarketplacePage component, allowing users to rescue ERC-721 and ERC-20 tokens.
    - Two new functions, rescueERC721 and rescueERC20, have been added to handle the opening of the corresponding modals.
    - Two new state variables, isOpenModalERC721 and isOpenModalERC20, have been added to control the visibility of the modals.
    - The modals, ModalRescueERC721 and ModalRescueERC20, have been included in the render method of the component, with their visibility controlled by the state variables.
    - The onClose and onModalClose functions have been implemented to close the modals when necessary.

--- src/app/admin/blockchain/marketplace/page.jsx
+++ src/app/admin/blockchain/marketplace/page.jsx
@@ -1,9 +1,33 @@
+'use client';
+
+import ModalRescueERC20 from '@/components/admin/modal/rescueERC20';
+import ModalRescueERC721 from '@/components/admin/modal/rescueERC721';
+import { useState } from 'react';
+
 const MarketplacePage = () => {
+  const [isOpenModalERC721, setIsOpenModalERC721] = useState(false);
+  const [isOpenModalERC20, setIsOpenModalERC20] = useState(false);
+
+  const rescueERC721 = () => {
+    setIsOpenModalERC721(true);
+  };
+
+  function closeModalERC721() {
+    setIsOpenModalERC721(false);
+  }
+
+  const rescueERC20 = () => {
+    setIsOpenModalERC20(true);
+  };
+
+  function closeModalERC20() {
+    setIsOpenModalERC20(false);
+  }
   return (
     <div>
       <div className="flex items-center justify-between py-5">
         <h3 className="text-3xl font-bold text-gray-900 dark:text-white">
-          Vault Contract Setting
+          Marketplace Contract Setting
         </h3>
       </div>
       <div className="mt-4 flex items-stretch justify-evenly gap-4">
@@ -26,6 +50,7 @@ const MarketplacePage = () => {
             <div class="border-t-2 border-neutral-100 px-6 py-3 dark:border-neutral-600 dark:text-neutral-50">
               <button
                 type="button"
+                onClick={rescueERC721}
                 class="mt-auto w-full rounded-full bg-primary-500 px-4 py-2 text-center text-sm font-bold text-white hover:bg-primary-300 disabled:cursor-not-allowed disabled:bg-primary-200 disabled:hover:bg-primary-200 dark:bg-white dark:text-neutral-700 dark:hover:bg-neutral-300 dark:disabled:bg-neutral-200 dark:disabled:text-neutral-100"
               >
                 Rescue ERC-721
@@ -53,6 +78,7 @@ const MarketplacePage = () => {
             <div class="border-t-2 border-neutral-100 px-6 py-3 dark:border-neutral-600 dark:text-neutral-50">
               <button
                 type="button"
+                onClick={rescueERC20}
                 class="mt-auto w-full rounded-full bg-primary-500 px-4 py-2 text-center text-sm font-bold text-white hover:bg-primary-300 disabled:cursor-not-allowed disabled:bg-primary-200 disabled:hover:bg-primary-200 dark:bg-white dark:text-neutral-700 dark:hover:bg-neutral-300 dark:disabled:bg-neutral-200 dark:disabled:text-neutral-100"
               >
                 Rescue ERC-20
@@ -61,6 +87,16 @@ const MarketplacePage = () => {
           </div>
         </div>
       </div>
+      <ModalRescueERC721
+        isOpenModal={isOpenModalERC721}
+        onClose={closeModalERC721}
+        onModalClose={closeModalERC721}
+      />
+      <ModalRescueERC20
+        isOpenModal={isOpenModalERC20}
+        onClose={closeModalERC20}
+        onModalClose={closeModalERC20}
+      />
     </div>
   );
 };

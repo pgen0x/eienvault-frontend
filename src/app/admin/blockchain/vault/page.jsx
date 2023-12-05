085d47bc7093937bb4681fe5474bc461fd1ada84
Ilham Nst <protogenox@gmail.com>
2023-12-05 17:39:33 +0700

    feat: Improve styling and functionality of vault page
    - Refactor class to className in JSX elements
    - Modify text and formatting in ERC-721 and ERC-20 token rescue sections
    - Update button styling and click handlers for rescue and withdrawal actions

--- src/app/admin/blockchain/vault/page.jsx
+++ src/app/admin/blockchain/vault/page.jsx
@@ -43,26 +43,26 @@ const VaultPage = () => {
       </div>
       <div className="mt-4 flex items-stretch justify-evenly gap-4">
         <div className="block flex-1 rounded-lg bg-white text-center dark:bg-neutral-900">
-          <div class="flex h-full flex-col rounded-lg bg-white text-center dark:bg-neutral-900">
-            <div class="border-b-2 border-neutral-100 px-6 py-3 text-xl font-medium text-neutral-700 dark:border-neutral-600 dark:text-neutral-50">
+          <div className="flex h-full flex-col rounded-lg bg-white text-center dark:bg-neutral-900">
+            <div className="border-b-2 border-neutral-100 px-6 py-3 text-xl font-medium text-neutral-700 dark:border-neutral-600 dark:text-neutral-50">
               Rescue ERC-721
             </div>
-            <div class="flex flex-grow flex-col justify-around p-4">
-              <h5 class="mb-2 text-lg font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+            <div className="flex flex-grow flex-col justify-around p-4">
+              <h5 className="mb-2 text-lg font-medium leading-tight text-neutral-800 dark:text-neutral-50">
                 Retrieve All ERC-721 Tokens
               </h5>
-              <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+              <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
                 This service is dedicated to retrieving all ERC-721 tokens
                 associated with a contract. Whether your tokens are stuck, lost,
                 or inaccessible, our solution ensures the comprehensive
                 retrieval of ERC-721 tokens.
               </p>
             </div>
-            <div class="border-t-2 border-neutral-100 px-6 py-3 dark:border-neutral-600 dark:text-neutral-50">
+            <div className="border-t-2 border-neutral-100 px-6 py-3 dark:border-neutral-600 dark:text-neutral-50">
               <button
                 type="button"
                 onClick={rescueERC721}
-                class="mt-auto w-full rounded-full bg-primary-500 px-4 py-2 text-center text-sm font-bold text-white hover:bg-primary-300 disabled:cursor-not-allowed disabled:bg-primary-200 disabled:hover:bg-primary-200 dark:bg-white dark:text-neutral-700 dark:hover:bg-neutral-300 dark:disabled:bg-neutral-200 dark:disabled:text-neutral-100"
+                className="mt-auto w-full rounded-full bg-primary-500 px-4 py-2 text-center text-sm font-bold text-white hover:bg-primary-300 disabled:cursor-not-allowed disabled:bg-primary-200 disabled:hover:bg-primary-200 dark:bg-white dark:text-neutral-700 dark:hover:bg-neutral-300 dark:disabled:bg-neutral-200 dark:disabled:text-neutral-100"
               >
                 Rescue ERC-721
               </button>
@@ -71,26 +71,26 @@ const VaultPage = () => {
         </div>
 
         <div className="block flex-1 rounded-lg bg-white text-center dark:bg-neutral-900">
-          <div class="flex h-full flex-col rounded-lg bg-white text-center dark:bg-neutral-900">
-            <div class="border-b-2 border-neutral-100 px-6 py-3 text-xl font-medium text-neutral-700 dark:border-neutral-600 dark:text-neutral-50">
+          <div className="flex h-full flex-col rounded-lg bg-white text-center dark:bg-neutral-900">
+            <div className="border-b-2 border-neutral-100 px-6 py-3 text-xl font-medium text-neutral-700 dark:border-neutral-600 dark:text-neutral-50">
               Rescue ERC-20
             </div>
-            <div class="flex flex-grow flex-col justify-around p-4">
-              <h5 class="mb-2 text-lg font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+            <div className="flex flex-grow flex-col justify-around p-4">
+              <h5 className="mb-2 text-lg font-medium leading-tight text-neutral-800 dark:text-neutral-50">
                 Retrieve All ERC-20 Tokens
               </h5>
-              <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+              <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
                 Our ERC-20 token rescue service is designed to efficiently
                 retrieve all ERC-20 tokens from a contract. If you&lsquo;re
                 facing challenges accessing your tokens, our solution ensures
                 the comprehensive recovery of ERC-20 tokens.
               </p>
             </div>
-            <div class="border-t-2 border-neutral-100 px-6 py-3 dark:border-neutral-600 dark:text-neutral-50">
+            <div className="border-t-2 border-neutral-100 px-6 py-3 dark:border-neutral-600 dark:text-neutral-50">
               <button
                 type="button"
                 onClick={rescueERC20}
-                class="mt-auto w-full rounded-full bg-primary-500 px-4 py-2 text-center text-sm font-bold text-white hover:bg-primary-300 disabled:cursor-not-allowed disabled:bg-primary-200 disabled:hover:bg-primary-200 dark:bg-white dark:text-neutral-700 dark:hover:bg-neutral-300 dark:disabled:bg-neutral-200 dark:disabled:text-neutral-100"
+                className="mt-auto w-full rounded-full bg-primary-500 px-4 py-2 text-center text-sm font-bold text-white hover:bg-primary-300 disabled:cursor-not-allowed disabled:bg-primary-200 disabled:hover:bg-primary-200 dark:bg-white dark:text-neutral-700 dark:hover:bg-neutral-300 dark:disabled:bg-neutral-200 dark:disabled:text-neutral-100"
               >
                 Rescue ERC-20
               </button>
@@ -99,24 +99,24 @@ const VaultPage = () => {
         </div>
 
         <div className="block flex-1 rounded-lg bg-white text-center dark:bg-neutral-900">
-          <div class="flex h-full flex-col rounded-lg bg-white text-center dark:bg-neutral-900">
-            <div class="border-b-2 border-neutral-100 px-6 py-3 text-xl font-medium text-neutral-700 dark:border-neutral-600 dark:text-neutral-50">
+          <div className="flex h-full flex-col rounded-lg bg-white text-center dark:bg-neutral-900">
+            <div className="border-b-2 border-neutral-100 px-6 py-3 text-xl font-medium text-neutral-700 dark:border-neutral-600 dark:text-neutral-50">
               Withdraw Fee Commission
             </div>
-            <div class="flex flex-grow flex-col justify-around p-4">
-              <h5 class="mb-2 text-lg font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+            <div className="flex flex-grow flex-col justify-around p-4">
+              <h5 className="mb-2 text-lg font-medium leading-tight text-neutral-800 dark:text-neutral-50">
                 Withdraw All Fee Commission
               </h5>
-              <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+              <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
                 This service is dedicated to withdraw all fee commission from
                 contract.
               </p>
             </div>
-            <div class="border-t-2 border-neutral-100 px-6 py-3 dark:border-neutral-600 dark:text-neutral-50">
+            <div className="border-t-2 border-neutral-100 px-6 py-3 dark:border-neutral-600 dark:text-neutral-50">
               <button
                 type="button"
                 onClick={withdrawAll}
-                class="mt-auto w-full rounded-full bg-primary-500 px-4 py-2 text-center text-sm font-bold text-white hover:bg-primary-300 disabled:cursor-not-allowed disabled:bg-primary-200 disabled:hover:bg-primary-200 dark:bg-white dark:text-neutral-700 dark:hover:bg-neutral-300 dark:disabled:bg-neutral-200 dark:disabled:text-neutral-100"
+                className="mt-auto w-full rounded-full bg-primary-500 px-4 py-2 text-center text-sm font-bold text-white hover:bg-primary-300 disabled:cursor-not-allowed disabled:bg-primary-200 disabled:hover:bg-primary-200 dark:bg-white dark:text-neutral-700 dark:hover:bg-neutral-300 dark:disabled:bg-neutral-200 dark:disabled:text-neutral-100"
               >
                 Withdraw Now
               </button>
@@ -128,11 +128,13 @@ const VaultPage = () => {
         isOpenModal={isOpenModalERC721}
         onClose={closeModalERC721}
         onModalClose={closeModalERC721}
+        type="vault"
       />
       <ModalRescueERC20
         isOpenModal={isOpenModalERC20}
         onClose={closeModalERC20}
         onModalClose={closeModalERC20}
+        type="vault"
       />
       <ModalWithdrawFee
         isOpenModal={isOpenModalWithdraw}
